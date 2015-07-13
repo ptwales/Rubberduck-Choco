5d44f31cca657cff3c948e423d13545041009286
Philip Wales <ptwales@gmail.com>
2015-07-13 15:17:25 -0500

    Finalized Uninstaller

--- Rubberduck/tools/chocolateyuninstall.ps1
+++ Rubberduck/tools/chocolateyuninstall.ps1
@@ -2,7 +2,6 @@
 
 $packageName = 'rubberduck'
 $registryUninstallerKeyName = '{979AFF96-DD9E-4FC2-802D-9E0C36A60D09}_is1' #ensure this is the value in the registry
-$msiProductCodeGuid = '{979AFF96-DD9E-4FC2-802D-9E0C36A60D09}'
 $shouldUninstall = $true
 
 $local_key       = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
@@ -16,22 +15,14 @@ $file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
     | Select-Object -ExpandProperty UninstallString
 
 if ($file -eq $null -or $file -eq '') {
-    Write-Host "$packageName has already been uninstalled by other means."
+    Write-Host "$packageName may have been uninstalled by other means."
     $shouldUninstall = $false
 }
 
-# The below is somewhat naive and built for EXE installers
 $installerType = 'EXE' 
 $silentArgs = '/SILENT'
 $validExitCodes = @(0)
 
-if (!(Test-Path $file)) {
-    Write-Host "$packageName has already been uninstalled by other means."
-    $shouldUninstall = $false
-}
-
 if ($shouldUninstall) {
  Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
 }
-
-HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{979AFF96-DD9E-4FC2-802D-9E0C36A60D09}_is1
